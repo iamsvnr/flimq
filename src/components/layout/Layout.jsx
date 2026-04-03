ccb2c6c424b8e3fc122bdcd3a7267d8ed5311d32
iamsvnr <iamsvnr@users.noreply.github.com>
2026-04-03 19:12:19 +0530

    Revert: restore navigation bar

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import Navbar from './Navbar';
 import Footer from './Footer';
 import ScrollToTop from '../ui/ScrollToTop';
 
@@ -9,6 +10,7 @@ export default function Layout() {
 
   return (
     <div className="min-h-screen flex flex-col">
+      <Navbar />
       <ScrollToTop />
       <main className="flex-1">
         <AnimatePresence mode="wait">
